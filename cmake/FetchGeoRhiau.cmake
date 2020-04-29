e27142c33fc1fbad9a58e0f6c68c3b653ee0f74f
Rose <rose@fppnt.com>
2020-04-29 18:25:37 +0100

    added georhiau and solvant via ExternalProject

---
+++ cmake/FetchGeoRhiau.cmake
@@ -0,0 +1,6 @@
+include(ExternalProject)
+
+ExternalProject_Add(georhiau
+	GIT_REPOSITORY "https://github.com/llyr-who/georhiau"
+	GIT_TAG origin/master
+)
---
+++ cmake/FetchSolvant.cmake
@@ -0,0 +1,7 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+  solvant
+  GIT_REPOSITORY https://github.com/llyr-who/solvant
+  GIT_TAG origin/master 
+  )
