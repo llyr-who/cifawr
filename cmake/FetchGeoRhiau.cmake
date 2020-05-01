0ab832036c6de5dce3d74b22c1160f265549c46a
llyr-who <antg3254@gmail.com>
2020-05-01 18:04:24 +0100

    added basic example that import external libraries

--- cmake/FetchGeoRhiau.cmake
+++ cmake/FetchGeoRhiau.cmake
@@ -1,6 +1,11 @@
 include(ExternalProject)
 
 ExternalProject_Add(georhiau
-	GIT_REPOSITORY "https://github.com/llyr-who/georhiau"
-	GIT_TAG origin/master
+  GIT_REPOSITORY "https://github.com/llyr-who/georhiau"
+  GIT_TAG origin/master
+  CONFIGURE_COMMAND ""
 )
+
+ExternalProject_Get_Property(georhiau install_dir)
+
+include_directories(${install_dir}/src/georhiau)
--- cmake/FetchSolvant.cmake
+++ cmake/FetchSolvant.cmake
@@ -5,3 +5,7 @@ ExternalProject_Add(
   GIT_REPOSITORY https://github.com/llyr-who/solvant
   GIT_TAG origin/master 
   )
+
+ExternalProject_Get_Property(solvant install_dir)
+
+include_directories(${install_dir}/src/solvant)
